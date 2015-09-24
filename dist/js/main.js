292a8f0496afb0edd9db699ab77d6c313c56c12b
Eugene1311 <ef7@mail.ru>
2015-09-24 20:51:15 +0300

    update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -46,7 +46,7 @@ function init() {
 			}, 5);
 		}
 		drawCircle();
-		}  
+	}  
 	constructCanvas('firstCanvas', 10, 'rgba(255, 0, 0, 1)');
 	constructCanvas('secondCanvas', 75, '#EFAC50');
 	constructCanvas('thirdCanvas', 45, '#5BB75D');
@@ -59,34 +59,90 @@ function init() {
 	};
 	App.Models.Item = Backbone.Model.extend({
 		defaults: {
-			checked: false
+			'checked': false
 		}
-	});
-	App.Collections.Items = Backbone.Collection.extend({});
-	App.Views.ItemsView = Backbone.View.extend({
-		el: '.sales_table',
+	}); 
+	App.Views.ItemView = Backbone.View.extend({
+		model: App.Models.Item,
+		template: _.template( $('#itemTpl').html() ),
 		initialize: function() {
-			this.listenTo(this.collection, 'change', this.render);
-			this.render();
+			//this.model.on('change: checked', this.checkedChanged, this);
+			this.listenTo(this.model, 'change: checked', this.checkedChanged);
 		},
 		events: {
-			'click .sort' : 'sort',
-			'click #headCheck' : 'editCheck',
-			'change .check' : 'changeCheck',
-			'click .control_delete' : 'removeModel'
+			'click .control_delete': 'delete',
+			'click .check': 'changeCheck',
+			'click .control_edit': 'edit'
 		},
-		removeModel: function(e) {
-			var target = e.target;
-			var id = $(target).closest('.sales_table_row').find('.check').attr('id');
-			this.collection.findWhere({mId: id}).destroy();
-			delete this['itemView' + id];
-			$(target).closest('.sales_table_row').remove();
+		checkedChanged: function() {
+			this.$el.find('.check').checked = this.model.get('checked');
 		},
-		changeCheck: function(e) {
-			var target = e.target;
-			var checked = target.checked,
-				id = target.id;
-			this.collection.findWhere({mId: id}).set('checked', checked);
+		delete: function() {
+			this.model.destroy();
+			this.remove();
+		},
+		changeCheck: function() {
+			var checked = this.el.querySelector('.check').checked;
+			this.model.set('checked', checked);
+		},
+		edit: function() {
+			var $input = $('<input type="text"/>');
+			$input.val(this.model.get('title'))
+					.addClass('product');
+			this.$el.find('td').eq(2).html('')
+									.append($input);
+			$input.focus();
+			var self = this;
+			$input.change(function() {
+				self.model.set('title', $(this).val());
+				$(this).blur();
+			});
+		},
+		render: function() {
+			this.$el.html(this.template(this.model.toJSON()));
+		}
+	});
+	App.Collections.Items = Backbone.Collection.extend({
+		model: App.Models.Item
+	});
+	App.Views.ItemsView = Backbone.View.extend({
+		el: '.sales_table',
+		ItemView: [],
+		initialize: function() {
+			this.collection.on('remove', this.updateInnerViews, this);
+			var that = this;
+			this.$el.find('.sales_table_row').each(function(i) {
+				var mId = $(this).find('td').eq(0).text(),
+					title = $(this).find('td').eq(2).text(),
+					category = $(this).find('td').eq(3).text(),
+					price = $(this).find('td').eq(4).text().slice(0, -1);
+				var model = new App.Models.Item({
+					mId: mId,
+					title: title,
+					category: category,
+					price: price
+				});
+				that.collection.add(model);
+				that.ItemView[i] = new App.Views.ItemView({model: model, el: $(this)});
+			});
+		},
+		events: {
+			'click #headCheck': 'checking',
+			'click .sort': 'sort'
+		},
+		updateInnerViews: function(model) {
+			var that = this;
+			_.each(this.ItemView, function(item, i) {
+				if (item.model === model) that.ItemView.splice(i, 1);
+			});
+		},
+		checking: function() {
+			var checked = this.el.querySelector('#headCheck').checked;
+			var that = this;
+			this.collection.each(function(item, i) {
+				item.set('checked', checked);
+				that.ItemView[i].render();
+			});
 		},
 		sort: function(e) {
 			var target = e.target;
@@ -119,59 +175,16 @@ function init() {
 				$(target).removeClass('decrease');
 				$(target).addClass('increase');
 			}
-			this.render();
+			this.bindModels();
 		},
-		editCheck: function(e) {
-			var target = e.target;
-			this.collection.each(function(item) {
-				item.set('checked', target.checked);
+		bindModels: function() {
+			var that = this;
+			[].forEach.call(this.ItemView, function(item, i) {
+				that.ItemView[i].model = that.collection.models[i];
+				that.ItemView[i].render();
 			});
-		},
-		render: function() {
-			this.collection.each(function(item, i) {
-				this['itemView' + i] = new App.Views.ItemView({
-					el: $('.sales_table tbody tr').eq(i),
-					model: item
-				});
-				this.$el.find('tbody').append(this['itemView' + i].render());
-			}, this);
-			return this;
 		}
 	});
-	App.Views.ItemView = Backbone.View.extend({
-		className: 'sales_table_row',
-		template: _.template( $('#itemTpl').html() ),
-		events: {
-			'click .control_edit' : 'edit',
-		},
-		edit: function() {
-			var $input = $('<input type="text"/>');
-			$input.val(this.model.get('title'))
-					.addClass('product');
-			this.$el.find('td').eq(2).html('')
-									.append($input);
-			$input.focus();
-			var self = this;
-			$input.change(function() {
-				self.model.set('title', $(this).val());
-			});
-		},
-		render: function() {
-			this.$el.html(this.template(this.model.toJSON()));
-			return this;
-		}
-	});
-	
 	App.Collections.items = new App.Collections.Items();
-	var rows = document.querySelectorAll('.sales_table_row');
-	[].forEach.call(rows, function(item) {
-		var model = new App.Models.Item({
-			mId: item.cells[0].innerHTML,
-			title: item.cells[2].innerHTML,
-			category: item.cells[3].innerHTML,
-			price: item.cells[4].innerHTML.slice(0, -1)
-		});
-		App.Collections.items.add(model);
-	});
 	App.Views.itemsView = new App.Views.ItemsView({collection: App.Collections.items});
 };
